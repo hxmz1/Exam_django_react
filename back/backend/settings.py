36cade2a4cb0af9a6ddb81c852d81e43054fc5ae
hamza ataoui <106755896+hxmz1@users.noreply.github.com>
2025-05-23 22:46:33 +0100

    version finale

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -165,8 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'hamza.ataoui@sesame.com.tn'
-#EMAIL_HOST_PASSWORD = 'vpgl qtxz tqdu ywsa'
+#EMAIL_HOST_USER = 'email@sesame.com.tn'
+#EMAIL_HOST_PASSWORD = '*********'
 
 #EMAIL_USE_TLS = True
 
